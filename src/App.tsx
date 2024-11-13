88fe23161d009706e1bd7e424e4a2f5eeb20bdd2
nidalA1992 <nidalakhmedjanov@gmail.com>
2024-11-13 13:14:23 +0500

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import "./App.css";
+import { Wrapper } from "sentoke-ui";
 
 function App() {
-  return <></>;
+  return (
+    <Wrapper bgCol={"primary"} r={"radiusB"} pm={"10px"}>
+      asdfasdf
+    </Wrapper>
+  );
 }
 
 export default App;
